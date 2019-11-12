40130a9f4e13fb5e7481db8a94b8efe64392089e
screendragons <chiyuyeung@outlook.com>
2019-11-12 13:12:08 +0100

    Bulletpoints added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,81 @@
-var currentIndex = 0,
-  items = $('.container div'),
-  itemAmt = items.length;
+jQuery(document).ready(function ($){
 
-function cycleItems() {
-  var item = $('.container div').eq(currentIndex);
-  items.hide();
-  item.css('display','inline-block');
-}
+  $(function() {
 
-var autoSlide = setInterval(function() {
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-}, 3000);
+    // the variable inWrap will be used in the class inner-wrapper
+    var inWrap = $('.inner-wrapper');
 
-$('.next').click(function() {
-  clearInterval(autoSlide);
-  currentIndex += 1;
-  if (currentIndex > itemAmt - 1) {
-    currentIndex = 0;
-  }
-  cycleItems();
-});
+    var bullet = $('.bullets .bullet');
 
-$('.prev').click(function() {
-  clearInterval(autoSlide);
-  currentIndex -= 1;
-  if (currentIndex < 0) {
-    currentIndex = itemAmt - 1;
-  }
-  cycleItems();
+    // previous button
+    // if you click on the previous button it will go back to the previous slider the .prev will target the inner-wrapper class
+    // which is defined in line 6
+    $('.prev').on('click', function()
+    {
+      // previous button (animation)
+      inWrap.animate({left: '0%'}, function()
+      {
+        // previous button (css)
+        // when the previous button is clicked, the slider will go to the left with -100%, it picks the css of the class
+        inWrap.css('left', '-100%');
+
+        // after the first slider it will go back to the last slider. Using the class slide, it targets all the classes with slide
+        // and in the class slide, there are the images
+        $('.slide').first().before($('.slide').last());
+      });
+    });
+
+
+    // next button
+    // if the next button is clicked
+    $('.next').on('click', function()
+    {
+      // next button (animation)
+      // the slide will go to the left with 200%, which means that you will see your second slide
+      inWrap.animate({left: '-200%'}, function()
+      {
+        // next button (css)
+        // when the next button is clicked, the slider will go to the move to the right, it picks the css of the class
+        inWrap.css('left', '-100%');
+
+        // after the last slider it will go back to the first slider. Using the class slide, it targets all the classes with slide
+        // and in the class slide, there are the images
+        $('.slide').last().after($('.slide').first());
+
+      });
+    });
+  });
+
+  // bulletpoints
+  bullet.on('click', function () {
+
+    // if you click on one of the bulletpoints, it will be active, if you click on another bulletpoint
+    // the class will be removed and another bulletpoint will be active
+    $(this).addClass('active').siblings().removeClass('active');
+
+    slide = $(this).data('slide');
+    if ($(this).hasClass('active')) {
+        slide.animate({'margin-left': (bullet.length - $(this).data('image')) * width }, slideClick);
+    }
+  });
+
+  // $("#controle1").click(function(e){
+  //     $("#1").fadeIn();
+  //     $("#2").fadeOut();
+  //     $("#3").fadeOut();
+  //   });
+
+  //   $("#controle2").click(function(e){
+  //     $("#1").fadeOut();
+  //     $("#2").fadeIn();
+  //     $("#3").fadeOut();
+  //   });
+
+  //   $("#controle3").click(function(e){
+  //     $("#1").fadeOut();
+  //     $("#2").fadeOut();
+  //     $("#3").fadeIn();
+  //   });
+
+
 });
